254c63a78a0625da95e4e8a8ada18139dff0c50f
XiaoPanPanKevinPan <XiaoPanPanKevinPan@users.noreply.github.com>
2023-08-12 19:42:31 +0800

    introduce css layer concept
    - and improve the styling of basicPage

--- site/basicPage/main.js
+++ site/basicPage/main.js
@@ -7,39 +7,75 @@
  *   <body><header /><main /><footer /></body>
  */
 
+// paths can be relative to this module or the page who imported this module
+const moduleBased = (str) => new URL(str, import.meta.url).toString();
+const pageBased = (str) => str;
+
 const headerHtml = `
 <div>
-	<a href="/"><h1>書法資料庫</h1></a>
+	<h1><a href="/">書法資料庫</a></h1>
 	<div id="menu">
 		<a href="/search">搜尋</a>
 	</div>
-</div>`;
+</div>
+<style> @import "${moduleBased("../general.css")}" </style>
+`;
 
 const footerHtml = `
 <div>
 	版權所有 blahblahblah。
 	建議使用最新版瀏覽器訪問本網站。
 </div>
+<style> @import "${moduleBased("../general.css")}" </style>
 `;
 
 const headerCss = `
+/* @import won't work here */
 
+h1 {
+	margin: 0;
+	font-weight: normal;
+}
+
+:host > div {
+	padding: var(--padding);
+
+	display: flex;
+	flex-flow: row wrap;
+	gap: 1em;
+
+	align-items: center;
+}
+
+:host {
+	background-color: white;
+	box-shadow: 0 0 0.5rem black;
+	margin-bottom: 0.5rem;
+}
+
+a {
+	color: inherit;
+	text-decoration: none;
+}
 `;
 
 const footerCss = `
+/* @import won't work here */
 
+:host {
+	border-top: solid 1px black;
+}
+
+:host > div {
+	display: flex;
+	flex-flow: row wrap;
+	padding: var(--padding);
+}
 `;
 
 // # here starts the script
-// script paths can be relative to this module or the page who imported this module
-const moduleBased = (str) => new URL(str, import.meta.url).toString();
-const pageBased = (str) => str;
-
-// import css
-fetch(moduleBased("./main.css"))
-	.then(res => res.text())
-	.then(css => new CSSStyleSheet({ baseURL: moduleBased("./main.css") }).replace(css))
-	.then(sheet => document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet]);
+// import css for the whole page
+document.head.insertAdjacentHTML("beforeend", `<style> @import "${moduleBased("./main.css")}" </style>`);
 
 // baseURL for css
 const baseURL = import.meta.url;
